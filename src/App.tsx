101ab3cd153aa414fd45c38d343803aa91dec0cc
Anthony Ponson <antonyponson@gmail.com>
2023-10-24 19:11:18 +0530

    first commit on youtube homwpage project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className='text-blue-600'>wetube</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
